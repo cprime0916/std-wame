13fa97120ce34d5d53ddd230acae4377786df507
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-08-12 22:57:18 +0800

    Update market.h
    Updated function arguments

--- market.h
+++ market.h
@@ -3,13 +3,15 @@
 
 #include <string>
 #include <vector>
-class markt{
-    public:
-        void shop(bool entered_shop);
-        void instruct(bool entered_shop, std::vector<std::string> items, std::vector<int> price, bool can_purchase, bool can_trade, bool can_sell);
-        void purchase(int index, int wallet, std::vector<int> price, std::vector<std::string> items, bool can_purchase, std::vector<bool> want_to);
-        void trade(std::vector<std::string> items, std::string item1, std::string item2, int index1, int index2, std::vector<int> worth_value, bool can_trade, std::vector<bool> want_to);
-        void sell(int index, int wallet, std::vector<int> price, std::vector<std::string> items, bool can_purchase, bool can_sale, std::vector<bool> want_to);
+#include <map>
+
+class markt {
+public:
+    void shop(bool& entered_shop);
+    void instruct(bool entered_shop, std::vector<std::string>& items, std::vector<int>& price, bool& can_purchase, bool& can_trade, bool& can_sell);
+    void purchase(int index, int wallet, std::vector<int>& price, std::vector<std::string>& items, bool can_purchase, std::vector<bool>& want_to);
+    void trade(std::vector<std::string>& items, std::string item1, std::string item2, int index1, int index2, std::map<std::string, int> worth_value, bool can_trade, std::vector<bool>& want_to);
+    void sell(int index, int& wallet, std::vector<int>& price, std::vector<std::string>& items, bool can_purchase, bool can_sell, std::vector<bool>& want_to);
 };
 
 #endif
