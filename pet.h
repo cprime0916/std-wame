d5a5dfb10d7929d23aab590b8565cd1fc1613604
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-08-12 18:59:25 +0800

    Update pet.h
    Added referencing to necessary variables

--- pet.h
+++ pet.h
@@ -5,13 +5,13 @@
 #include <map>
 #include <vector>
 
-class pet{
+class pet {
     bool generate(double lowerBound, double upperBound, double probability);
-    void tame(std::string pet, char rarity, std::map<std::string, bool> taming_list);
-    void command_pet(std::string pet, int mode, int xp, bool stay, bool follow, bool hunt, bool add_mode);
-    void utk(std::string pet, int pet_level, char rarity, int o_health);
-    void feed(std::string pet, std::string food, bool want_to[], bool have_in_invn[], int index, int salv);
-    void free_pet(std::string pet, std::map<std::string, bool> taming_list, bool game_loop);
+    void tame(const std::string& pet, char rarity, std::map<std::string, bool>& taming_list);
+    void command_pet(const std::string& pet, int mode, int xp, bool& stay, bool& follow, bool& hunt, bool& add_mode);
+    void utk(const std::string& pet, int pet_level, char rarity, int& o_health);
+    void feed(const std::string& pet, const std::string& food, bool want_to[], bool have_in_invn[], int index, int& salv);
+    void free_pet(const std::string& pet, std::map<std::string, bool>& taming_list, bool& game_loop);
 };
 
 #endif
