9b3ee9a2239504096ce767454375f819b707c62a
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-08-26 12:12:36 +0800

    Edit item.h
    Added the end() function to deactivate item mode

--- src/item.h
+++ src/item.h
@@ -11,6 +11,7 @@ class item{
         void del(std::vector<std::string>& items, int index, std::string item, bool item_mode);
         void use(std::vector<std::string> items, std::vector<bool>& use, std::string item, int index, bool item_mode);
         void empty(std::vector<std::string>& items, bool item_mode, bool& game_loop);
+        void end(bool& item_mode);
 };  
 
 #endif
