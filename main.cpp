9c9fe5dc8a1796d3e75e6176907fa62fc0b567e4
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-08-26 14:06:02 +0000

    Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 int main(){
     bool game_loop = true;
     while(game_loop){
-        
+        std::string command;
+        getline(std::cin, command);
+        // First Error Handling (Later will be in ErrorHandling() function)
+        if(command[0] != '/') std::cout << "Error 101: Invalid Command" << std::endl;
+        else{
+            try{
+                // game code
+            } catch(const std::exception& e){
+                std::cout << "Error 102: Unexecutable Console" << std::endl;
+            }
+        }
     }
 }
