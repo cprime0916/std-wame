67ec8486829cdefadb07e3673bdd810a74d24626
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-08-27 11:35:36 +0800

    Added game header file with all necessary functions

---
+++ game/game.h
@@ -0,0 +1,10 @@
+#ifndef GAME_H
+#define GAME_H
+#include "src/fight/fight.h"
+#include "src/item/item.h"
+#include "src/macro/dictionary.h"
+#include "src/macro/chungdin.h"
+#include "src/market/market.h"
+#include "src/pet/pet.h"
+#include "src/system/system.h"
+#endif
