abb274110db07e88b0055ceb15bb9b3c33a1b70f
cprime0916 <137139743+cprime0916@users.noreply.github.com>
2023-08-28 14:00:03 +0800

    changed the name of "system" to "mechanism", in order to not be confused with system()

---
+++ src/mechanism/mechanism.cpp
@@ -0,0 +1,9 @@
+#include "mechanism.h"
+
+void mech::load(std::string what_the_fuck_am_I_doing){
+
+}
+
+void mech::save(){
+
+}
---
+++ src/mechanism/mechanism.h
@@ -0,0 +1,10 @@
+#ifndef MECHANISM_H
+#define MECHANISM_H
+#include <iostream>
+#include <string>
+class mech{
+    public:
+        void load(std::string what_the_fuck_am_I_doing);
+        void save();
+};
+#endif
